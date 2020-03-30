e1ac0304dd6aec90391387c65e85d339539f628c
Martin Gerstmaier <Gerstmaier.Martin@gmx.net>
2020-03-30 21:57:46 +0200

    arrow funtion to get data fetching to work

--- src/utils/DataLoader.js
+++ src/utils/DataLoader.js
@@ -7,48 +7,39 @@ const SUM_UP_PARAM = '&onlyCountries=false';
 const countryList = require('country-list');
 const countries = countryList.getCodes();
 
+const axios = require('axios').default;
+
 class DataLoader {
     data = {};
 
     constructor() {
         console.log('Setup dataloader');
+        this.set_data = this.set_data.bind(this);
         countries.forEach(this.fetch_data);
     }
 
     fetch_data = (country) => {
         console.log('Load data for', country);
         let searchTerm = COUNTRY_QUERY + country + SUM_UP_PARAM;
-        fetch(searchTerm, {
-            method: 'GET', 
-            })
-            .then(response => response.json())
-            .then(result => this.set_data(result))
-            .catch(e => console.log(e));
-        }
-
-    set_data(result) {
-        console.log('Fill data');
-        // let country = result.countryregion;
-        // let location = result.location;
-        // let countrycode = result.countrycode;
-        // let timeseries = result.timeseries;
-
-        // timeseries.forEach(this.fill_data);
+        axios.get(searchTerm)
+            .then(response => this.set_data(response.data[0]))
+            .catch(e => e);
     }
 
-    // fetch_data(country) {
-    //     console.log('Load data for', country);
-    //     let searchTerm = COUNTRY_QUERY + country + SUM_UP_PARAM;
-    //     axios.get(searchTerm)
-    //         // .then(response => console.log(response.data[0]))
-    //         .then(response => this.set_data(response))
-    //         .catch(e => e);
-    // }
-
-
+    set_data(result) {
+        // console.log(result);
+        let country = result.countryregion;
+        let location = result.location;
+        let countrycode = result.countrycode;
+        let timeseries = result.timeseries;
+        // console.log(timeseries);
+        for (const [key, value] of Object.entries(timeseries)) {
+            console.log(key, value);
+          }
+    }
 
     fill_data(cur_data) {
-        console.log(cur_data);
+        // console.log(cur_data);
         // let timestamp = cur_data.
         // if !(this.data[])
     }
