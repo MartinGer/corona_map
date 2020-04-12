5439d226dfb5897844614c410f6b2177f86a1225
Martin Gerstmaier <Gerstmaier.Martin@gmx.net>
2020-04-12 21:57:44 +0200

    add kova and try creating a color bar

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -3,8 +3,8 @@ import './map.css';
 import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 
 import PropTypes from 'prop-types';
+import { Stage, Layer, Rect } from 'react-konva';
 import geoJSON from '../../data/countries.geo.json';
-
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default class Maps extends Component {
@@ -61,6 +61,10 @@ export default class Maps extends Component {
 
     return (
       <div className="Map">
+        <GradientBar
+          width={"20vh"}
+          height={"100vh"}
+        />
         <Map
           center={[lat, lng]}
           zoom={zoom}
@@ -96,3 +100,47 @@ Maps.propTypes = {
   curDate: PropTypes.string,
   populationData: PropTypes.objectOf(PropTypes.number),
 };
+
+const GradientBar = ({ width, height }) => (
+  <Stage width={width} height={height}>
+    <Layer>
+      <Rect
+        width={100}
+        height={height}
+        fill="red"
+        shadowBlur={5}
+      />
+    </Layer>
+  </Stage>
+
+  // <div id="container"></div>
+  // var width = window.innerWidth;
+  // var height = window.innerHeight;
+  // stage = new Konva.Stage({
+  //   container: 'container',
+  //   width: width,
+  //   height: height
+  // });
+
+  // var layer = new Konva.Layer();
+
+  // var rect1 = new Konva.Rect({
+  //   x: 20,
+  //   y: 20,
+  //   width: 100,
+  //   h eight: 50,
+  //   fill: 'green',
+  //   stroke: 'black',
+  //   strokeWidth: 4
+  // });
+  // // add the shape to the layer
+  // layer.add(rect1);
+  // stage.add(layer);
+  // // <View
+  // //   style={{
+  // //     height: 1,
+  // //     backgroundColor: 'rgba(255, 255, 255 ,0.3)',
+  // //     alignSelf: 'stretch',
+  // //   }}
+  // // />
+);
