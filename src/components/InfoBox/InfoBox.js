e63668d0372dd7d04e15165c56145f6e15a998a8
Martin Gerstmaier <Gerstmaier.Martin@gmx.net>
2020-04-21 20:16:21 +0200

    fix css for InfoBox

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -16,43 +16,41 @@ export default function InfoBox({ countryData, population }) {
     <div>
       {!(Object.keys(countryData).length === 0) && population
         ? (
-          <div className="infoBox">
-            <div style={{ textAlign: 'left' }}>
-              <h4>{country}</h4>
-              <div className="totalLine">
-                Total Cases:
-                {' '}
-                {(confirmed || '-').toLocaleString()}
-              </div>
-              <div className="bar">
-                <div className="slice" style={{ background: 'rgb(255, 64, 0)', width: `${infectedPercentage}%`, marginRight: '4px' }} />
-                <div className="slice" style={{ background: 'rgb(96, 187, 105)', width: `${recoveredPercentage}%`, marginRight: '4px' }} />
-                <div className="slice" style={{ background: 'rgb(0, 0, 0)', width: `${deathsPercentage}%` }} />
-              </div>
-              <div className="infoLine">
-                <div className="circle" style={{ background: 'rgb(255, 64, 0)' }} />
-                <div className="infoLineText">Currently Infected:</div>
-                <div className="infoLineData">{(infected || '-').toLocaleString()}</div>
-              </div>
-              <div className="infoLine">
-                <div className="circle" style={{ background: 'rgb(96, 187, 105)' }} />
-                <div className="infoLineText">Recovered:</div>
-                <div className="infoLineData">{(recovered || '-').toLocaleString()}</div>
-              </div>
-              <div className="infoLine">
-                <div className="circle" style={{ background: 'rgb(0, 0, 0)' }} />
-                <div className="infoLineText">Deaths:</div>
-                <div className="infoLineData">{(deaths || '-').toLocaleString()}</div>
-              </div>
-              <div className="percentageLine">
-                ~
-                {((infected / population) * 100).toFixed(2)}
-                % of
-                {' '}
-                {population.toLocaleString()}
-                {' '}
-                people are currently infected
-              </div>
+          <div className="infoBox" style={{ textAlign: 'left' }}>
+            <h4>{country}</h4>
+            <div className="totalLine">
+              Total Cases:
+              {' '}
+              {(confirmed || '-').toLocaleString()}
+            </div>
+            <div className="bar">
+              <div className="slice" style={{ background: 'rgb(255, 64, 0)', width: `${infectedPercentage}%`, marginRight: '4px' }} />
+              <div className="slice" style={{ background: 'rgb(96, 187, 105)', width: `${recoveredPercentage}%`, marginRight: '4px' }} />
+              <div className="slice" style={{ background: 'rgb(0, 0, 0)', width: `${deathsPercentage}%` }} />
+            </div>
+            <div className="infoLine">
+              <div className="circle" style={{ background: 'rgb(255, 64, 0)' }} />
+              <div className="infoLineText">Currently Infected:</div>
+              <div className="infoLineData">{(infected || '-').toLocaleString()}</div>
+            </div>
+            <div className="infoLine">
+              <div className="circle" style={{ background: 'rgb(96, 187, 105)' }} />
+              <div className="infoLineText">Recovered:</div>
+              <div className="infoLineData">{(recovered || '-').toLocaleString()}</div>
+            </div>
+            <div className="infoLine">
+              <div className="circle" style={{ background: 'rgb(0, 0, 0)' }} />
+              <div className="infoLineText">Deaths:</div>
+              <div className="infoLineData">{(deaths || '-').toLocaleString()}</div>
+            </div>
+            <div className="percentageLine">
+              ~
+              {((infected / population) * 100).toFixed(2)}
+              % of
+              {' '}
+              {population.toLocaleString()}
+              {' '}
+              people are currently infected
             </div>
           </div>
         )
