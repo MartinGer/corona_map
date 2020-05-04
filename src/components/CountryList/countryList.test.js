2b554d9fc3f863d3a60244e2a1b0f7efc62be4d9
Martin Gerstmaier <Gerstmaier.Martin@gmx.net>
2020-05-04 12:58:29 +0200

    remove doubled files

--- src/components/CountryList/countryList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CountryList from './CountryList';
-
-describe('test CountryList component', () => {
-  it('renders without crashing', () => {
-    shallow(<CountryList />);
-  });
-});
--- src/components/Map/Map.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Map from './map';
-
-describe('test Map component', () => {
-  it('renders without crashing', () => {
-    shallow(<Map />);
-  });
-});
